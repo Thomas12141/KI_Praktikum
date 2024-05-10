df63636c7f13237354aebf81117555eca3c0cc47
thomas <fidorinthomas@gmail.com>
2024-05-10 20:16:22 +0200

    minimax

--- Aufgabe3/main.py
+++ Aufgabe3/main.py
@@ -1,7 +1,8 @@
+from Aufgabe8.minimax import MinimaxPlayer
 from game_controller import GameControllerConnectFour
 from board_model import ConnectFourBoard, Cell
 from game_model import ConnectFour
-from player_model import HumanPlayer, RandomPlayer, UniformCostPlayer, UniformCostSimplePlayer
+from player_model import HumanPlayer, RandomPlayer, UniformCostPlayer, UniformCostSimplePlayer, prepare_game
 from game_view import GameViewConnectFour
 
 
@@ -9,14 +10,13 @@ def main():
     board = ConnectFourBoard()
     game = ConnectFour(board)
     view = GameViewConnectFour()
-    player_c = UniformCostSimplePlayer(game)
-
+    player_a = HumanPlayer("X", "Thomas", game)
+    player_b = MinimaxPlayer(game,"O", "X")
+    prepare_game(game, "X", "O")
     controller = GameControllerConnectFour(game, view)
-    controller.start()
-
-    whos_turn = player_c
+    whos_turn = player_a
     while True:
-
+        view.draw(board)
         controller._play_one_round(whos_turn)
         if game.check_win(whos_turn.get_symbol()):
             print("The winner is " + whos_turn.get_symbol() + ".")
@@ -24,7 +24,7 @@ def main():
         if board.is_full():
             print("The game is over, without a winner.")
             break
-
+        whos_turn = player_a if whos_turn == player_b else player_b
 
 if __name__ == '__main__':
     main()
